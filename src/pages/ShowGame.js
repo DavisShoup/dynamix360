973eaf345a191798a4d57a890dcd553be7a92c28
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-06-21 11:30:47 -0400

    added api key

--- src/pages/ShowGame.js
+++ src/pages/ShowGame.js
@@ -1,73 +1,64 @@
-import { useState } from 'react'
 
-const Create = (props) => {
-    const [ newGame, setNewGame ] = useState({
-        name: "",
-        sport: "",
-        difficulty: "",
-        location: "",
-        description: "",
-    })
+
+import { useState } from 'react';
+
+const ShowGame = (props) => {
+    const id = props.match.params.id;
+    const event = props.game.find(p => p._id === id);
+
+    const [ editGame, setEditGame ] = useState(event);
 
     const handleChange = (e) => {
-        setNewGame({
-            ...newGame,
+        setEditGame({
+            ...editGame,
             [e.target.name]: e.target.value
-        });
+        })
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        props.createGame(newGame);
+        const { name, sport, difficulty, location, description, _id } = editGame;
+        props.updateGame({ name, sport, difficulty, location, description }, _id);
     }
 
-    return (
+    const handleRemoveGame = (id) => {
+        props.deleteGame(id);
+        props.history.push('/search')
+    }
+
+    return(
         <section>
             <form onSubmit={handleSubmit}>
                 <input 
-                value={newGame.name} 
+                value={editGame.name} 
                 onChange={handleChange} 
                 placeholder="name"
                 name="name" 
                 type="text" 
                 />
                 <input 
-                value={newGame.sport} 
+                value={editGame.sport} 
                 onChange={handleChange}
                 placeholder="sport" 
                 name="sport" 
                 type="text" 
                 />
                 <input 
-                value={newGame.difficulty}
+                value={editGame.difficulty}
                 onChange={handleChange}
                 placeholder="difficulty"
                 name="difficulty" 
                 type="text" 
                 />
                 <input 
-                value={newGame.location}
+                value={editGame.location}
                 onChange={handleChange}
                 placeholder="location"
                 name="location" 
                 type="text" 
                 />
                 <input 
-                value={newGame.date}
-                onChange={handleChange}
-                placeholder="date"
-                name="date" 
-                type="text" 
-                />
-                <input 
-                value={newGame.time}
-                onChange={handleChange}
-                placeholder="time"
-                name="time" 
-                type="text" 
-                />
-                <input 
-                value={newGame.description}
+                value={editGame.description}
                 onChange={handleChange}
                 placeholder="description"
                 name="description" 
@@ -78,7 +69,7 @@ const Create = (props) => {
                 value="Create Game" />
             </form>
         </section>   
-    );
-};
+    )
+}
 
-export default Create
+export default ShowGame
