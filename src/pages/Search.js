f6a7b9a20a2cde56b20acea2fbd502daf704b001
Davis Shoup <davisshoup@yahoo.com>
2022-06-21 11:17:34 -0400

    API key access.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,12 @@
+import { useState, useMemo, useCallback, useRef } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import Map from '../components/Map.js'
+import Places from '../components/Places.js'
 
 
 const Search = ({game}) => {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    });
-
-
+    const [park, setPark] = useState();
     const gameInfo = () => {
         let gamesArray = [];
         for (let i = 0; i < game.length; i++) {
@@ -16,20 +16,39 @@ const Search = ({game}) => {
     };
 
     const Map = () => {
-        return <GoogleMap
-            zoom={5}
-            center={{lat: 40, lng: -100}}
-            mapContainerClassName="map-container">
-            <Marker position={{lat: 40, lng: -100}}/>
-        </GoogleMap>
-        
+        const mapRef = useRef();
+        const center = useMemo(() => ({lat: 40, lng: -100}), []);
+        const options = useMemo(() => ({
+            mapId: "b6ef39514783a2a4",
+            disableDefaultUI: true,
+            clickableIcons: false,
+        }), []);
+        const onLoad = useCallback(map => (mapRef.current = map), []);
+
+        return(
+            <div className="container">
+            <div className="controls">
+                <h1>Games</h1>
+            </div>
+            <div className="map">
+            <GoogleMap
+                zoom={5}
+                center={{lat: 40, lng: -100}}
+                mapContainerClassName="map-container"
+                options={options}
+                onLoad={onLoad}>
+            </GoogleMap>
+            </div>
+        </div>
+        )
     }
 
-    if (!isLoaded) return <h1>Loading ...</h1>
     return(
-        <div className="games">
-            {Map()}
-            {gameInfo()}
+        <div>
+            <Wrapper apiKey='your-api-key'>
+                {Map()}
+            </Wrapper>
+                {gameInfo()}
         </div>
     )
 };
