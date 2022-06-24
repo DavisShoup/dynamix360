73fd89389a1a71a8621c149ec8ee15273d70da7e
Davis Shoup <davisshoup@yahoo.com>
2022-06-24 15:35:18 -0400

    final change.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,25 +1,11 @@
-import { Link } from "react-router-dom"
-import Map from "../components/Map";
+import GameInfo from "../components/gameInfo";
 
 const Search = ({game}) => {
-    // const [park, setPark] = useState();
-    const gameInfo = () => {
-        let gamesArray = [];
-        for (let i = 0; i < game.length; i++) {
-            gamesArray.push(
-                <Link to={`/game/${game[i]._id}`}>
-            <h1>{game[i].name}</h1>
-            </Link>)
-        }
-        return gamesArray;
-    }
-
     return(
         <div className="search-container">
 
             <h1>All Games</h1>
-            <Map />
-                {gameInfo()}
+            <GameInfo game={game} />
                 </div>
 
     )
